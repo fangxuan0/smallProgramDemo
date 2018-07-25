84b948e2a33abd60923abc039873e362a28141bd
siweihong <siweihong@doumi.com>
2018-07-25 20:34:06 +0800

    增加搜索电影功能

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,9 +1,12 @@
 //logs.js
 const util = require('../../utils/util.js')
+const app = getApp()
+
+var API_URL = "http://t.yushu.im/v2/movie/search"
 
 Page({
   data: {
-    logs: []
+    list: []
   },
   onLoad: function () {
     this.setData({
@@ -12,13 +15,51 @@ Page({
       })
     })
   },
-  onReady:()=>{
+  onReady : ()=>{
     wx.setNavigationBarTitle({
-      title: '新设置的',
+      title: '搜索',
     })
   },
-  onReachBottom: (e) => {
-    console.log(e)
-    console.log('上啦')
+
+  /**
+   * 显示电影详情页
+   */
+  goToMovieDetail: function (e) {
+    // console.log(e.currentTarget.dataset.index);
+    const index = e.currentTarget.dataset.index
+    let item = this.data.list[index]
+    app.globalData.duanDetail = item;
+    wx.navigateTo({
+      url: '../movieDetail/movieDetail'
+    })
   },
+
+  /**
+   * 搜索电影
+   */
+  search : function (e){
+    var that = this;
+    if(!e.detail.value){
+      return;
+    }
+    wx.showLoading({
+      title: '加载中...',
+      mask: true
+    })
+    wx.request({
+      url: API_URL + "?q=" + e.detail.value,
+      data: {},
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res)
+        wx.hideLoading();
+        that.setData({
+          list : res.data.subjects
+        })
+      }
+    })
+  }
 })
